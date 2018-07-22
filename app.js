dacb855daa8795d8390134181550888aa53b300c
Tareq Ahamed <tareq.ahamed@outlook.com>
2018-07-22 20:38:30 +0600

    port

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const AnyProxy = require('anyproxy');
 const options = {
-  port: process.env.PORT || '8001',
+  port: 8001, // process.env.PORT || 
   // rule: require('myRuleModule'),
   webInterface: {
     enable: true,
-    // webPort: 8002
+    webPort: 8002
   },
   throttle: 10000,
   forceProxyHttps: false,
@@ -12,7 +12,7 @@ const options = {
   silent: false
 };
 const proxyServer = new AnyProxy.ProxyServer(options);
-console.log('running port ==================> ' + process.env.PORT);
+
 proxyServer.on('ready', () => { /* */ });
 proxyServer.on('error', (e) => { /* */ });
 proxyServer.start();
