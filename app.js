ac374e9d409b96cc1231cf062b71ea196f4095f3
Tareq Ahamed <tareq.ahamed@outlook.com>
2018-07-22 20:00:09 +0600

    port...

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const AnyProxy = require('anyproxy');
 const options = {
-  port: 8001,
-//   rule: require('myRuleModule'),
+  port: process.env.PORT || '8001',
+  // rule: require('myRuleModule'),
   webInterface: {
     enable: true,
-    webPort: 8002
+    // webPort: 8002
   },
   throttle: 10000,
   forceProxyHttps: false,
@@ -12,10 +12,10 @@ const options = {
   silent: false
 };
 const proxyServer = new AnyProxy.ProxyServer(options);
-
+console.log('running port ==================> ' + process.env.PORT);
 proxyServer.on('ready', () => { /* */ });
 proxyServer.on('error', (e) => { /* */ });
 proxyServer.start();
 
 //when finished
-proxyServer.close();
+// proxyServer.close();
