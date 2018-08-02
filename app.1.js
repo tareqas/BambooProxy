4fe84fd4400257ba32713403892710cc9e182b56
Tareq Ahamed <tareq.ahamed@outlook.com>
2018-08-02 12:13:27 +0600

    added straightforward proxy

---
+++ app.1.js
@@ -0,0 +1,21 @@
+const AnyProxy = require('anyproxy');
+const options = {
+  port: 8001, // process.env.PORT || 
+  // rule: require('myRuleModule'),
+  webInterface: {
+    enable: true,
+    webPort: 8002
+  },
+  throttle: 10000,
+  forceProxyHttps: false,
+  wsIntercept: false,
+  silent: false
+};
+const proxyServer = new AnyProxy.ProxyServer(options);
+
+proxyServer.on('ready', () => { /* */ });
+proxyServer.on('error', (e) => { /* */ });
+proxyServer.start();
+
+//when finished
+// proxyServer.close();
--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-const AnyProxy = require('anyproxy');
-const options = {
-  port: 8001, // process.env.PORT || 
-  // rule: require('myRuleModule'),
-  webInterface: {
-    enable: true,
-    webPort: 8002
-  },
-  throttle: 10000,
-  forceProxyHttps: false,
-  wsIntercept: false,
-  silent: false
-};
-const proxyServer = new AnyProxy.ProxyServer(options);
-
-proxyServer.on('ready', () => { /* */ });
-proxyServer.on('error', (e) => { /* */ });
-proxyServer.start();
-
-//when finished
-// proxyServer.close();
+const Straightforward = require('straightforward')
+ 
+;(async () => {
+  // Start proxy server
+  const sf = await new Straightforward().listen(8080)
+  console.log(`Proxy listening on http://localhost:9191(8080)`)
+ 
+  // Log http requests
+  sf.onRequest(async ({ req, res }, next) => {
+    console.log(`http request: ${req.url}`)
+    // Note the common middleware pattern, use `next()`
+    // to pass the request to the next handler.
+    return next()
+  })
+ 
+  // Log connect (https) requests
+  sf.onConnect(async ({ req, res }, next) => {
+    console.log(`connect request: ${req.url}`)
+    return next()
+  })
+ 
+})()
